1b11dc8753471b11dbe8daa3ef7b59d6daae7a40
hgarcia <hernan@dynamicprogrammer.com>
2013-05-14 18:05:53 -0400

    Custom links, fix youtu.be import error.

--- public/javascripts/videos.js
+++ public/javascripts/videos.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
 
-  function clickHandler(ev) {
-    var provider = this.getAttribute("data-provider");
-    var urlParts = this.getAttribute("data-url").split("/");
-    var videoKey = urlParts[urlParts.length - 1];
-    var h = "";
+  function notFound(){
+    console.log("404 Not Found");
+  }
+
+  Path.map("/videos/:videoKey/:provider/:title").to(function () {
+    var h = getVideoHtml(this.params["videoKey"], this.params["provider"]);
+    $('#player').html(h);
+  });
+  Path.root("/videos");
+  Path.rescue(notFound);
+  Path.history.listen(true);
+
+  function getVideoHtml(videoKey, provider) {
     if (provider === "youtube") {
-      h = '<iframe width="90%" height="392" src="http://www.youtube.com/embed/' + videoKey + '?feature=oembed" frameborder="0" allowfullscreen></iframe>';
+      return '<iframe width="90%" height="392" src="http://www.youtube.com/embed/' + videoKey + '?feature=oembed" frameborder="0" allowfullscreen></iframe>';
     }
     if (provider === "vimeo") {
-      h = '<iframe src="http://player.vimeo.com/video/' + videoKey + '?title=0&amp;byline=0&amp;portrait=0&amp;color=3B65AF" width="100%" height="392" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+      return '<iframe src="http://player.vimeo.com/video/' + videoKey + '?title=0&amp;byline=0&amp;portrait=0&amp;color=3B65AF" width="100%" height="392" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
     }
-    $('#player').html(h);
+  }
+
+  function clickHandler(event) {
+    event.preventDefault();
+    Path.history.pushState({}, "", $(this).attr("href"));
   }
 
   function list(videos) {
@@ -26,6 +38,8 @@ $(document).ready(function () {
       var img = document.createElement('img');
       var h4 = document.createElement('h4');
       var span = document.createElement('span');
+      var urlParts = v.video_url.split("/");
+      var videoKey = urlParts[urlParts.length - 1];
       img.src = v.thumbnail_url;
       img.alt = v.title + ' by ' + v.author_name;
       img.setAttribute("class", provider_name + "-thumb");
@@ -34,12 +48,10 @@ $(document).ready(function () {
       h4.innerHTML = v.title;
       h4.title = v.title;
       span.innerHTML = "(" + v.author_name + ")";
-      a.href = "#";
-      a.title = img.alt;
-      a.setAttribute("data-url", v.video_url);
-      a.setAttribute("data-provider", provider_name);
-      a.appendChild(imgCtr);
+      a.href = "/videos/" + videoKey + "/" + provider_name + "/" + v.title;
       a.onclick = clickHandler;
+      a.title = img.alt;
+      a.appendChild(imgCtr);
       a.appendChild(h4);
       a.appendChild(span)
       li.appendChild(a);
