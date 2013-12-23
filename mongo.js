48df5599bd5c538ce528649372e09ac363272992
hgarcia <hernan@dynamicprogrammer.com>
2013-12-23 18:50:09 -0500

    [HG] Fixing spacing.

--- mongo.js
+++ mongo.js
@@ -4,8 +4,8 @@ exports.create = function (config) {
     return mongo.db(config.url + '/?autoReconnect=true');
   } else {
     return mongo.db(
-                    config.uris[0] + '/' + config.options.database + '/?autoReconnect=true',
-                    config.options
-                  );
+      config.uris[0] + '/' + config.options.database + '/?autoReconnect=true',
+      config.options
+    );
   }
 };
